291ced029f1a7b2bd6a5f20e9370acb071f941eb
ubuntu <ubuntu@example.com>
2025-11-24 00:15:06 +0200

    Stuff.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+export default {
+  semi: false,
+  singleQuote: false,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  // Configuration for the sort import plugin.
+  // See: https://github.com/IanVS/prettier-plugin-sort-imports/tree/v4.7.0?tab=readme-ov-file#options
+  importOrder: ["^node:", "^[a-z]", "^\\./", "^\\.\\./"],
+}
 
 
 
